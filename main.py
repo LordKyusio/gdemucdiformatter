52bcd2326f59cc2cd92e88bbe8761113d4f71814
klusek <klusek@MacBook-Pro-MacBook.local>
2022-08-26 13:04:09 +0200

    Add ISO support

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import glob
 
 def gdi_file_formatter():
     folders = os.listdir(os.curdir)
+    supported_formats = ['cdi', 'iso']
+    input_files = []
     if 'INPUT' not in folders:
         print('INPUT dir not found. Please make sure that INPUT folder exists')
         return
@@ -12,12 +14,13 @@ def gdi_file_formatter():
         print('OUTPUT dir not found')
         os.mkdir('OUTPUT')
         print('OUTPUT created in current directory')
+    for supported_format in supported_formats:
+        input_files += (glob.glob(os.getcwd() + f'/I*/**/*.{supported_format}', recursive=True))
 
-    input_files = glob.glob(os.getcwd() + '/I*/**/*.cdi', recursive=True)
     output_folders = os.listdir(os.curdir + '/OUTPUT')
     output_files = glob.glob(os.getcwd() + '/OUTPUT', recursive=True)
 
-    folder_name = ''
+    folder_name = None
 
     n = 2  # folder 01 is reserved for GDEMU settings file
     for i in range(len(input_files)):
@@ -34,8 +37,9 @@ def gdi_file_formatter():
 
         os.mkdir('{}/{}'.format(output_files[0], folder_name))
         output_folders = os.listdir(os.curdir + '/OUTPUT')
-        shutil.copy(input_files[i], '{}/{}/{}'.format(output_files[0], folder_name, 'disc.cdi'))
-        print('Moved {} to {}'.format(input_files[i], '{}/{}/{}'.format(output_files[0], folder_name, 'disc.cdi')))
+        file_format = input_files[i][-3:]
+        shutil.copy(input_files[i], '{}/{}/{}'.format(output_files[0], folder_name, f"disc.{file_format}"))
+        print('Moved {} to {}'.format(input_files[i], f"{output_files[0]}/{folder_name}/{f'disc.{file_format}'}"))
         n += 1
 
 
