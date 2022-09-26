405dadbcae63f7e0b25ec3f98395c2845fdac75f
LordKyusio <75700120+LordKyusio@users.noreply.github.com>
2022-09-26 23:59:17 +0200

    Update folder name loop General Refactor (#4)
    * Update folder name loop
    * General Refactor

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import os
 import shutil
 import glob
+import typing
+
+BASE_PATH = os.path.abspath(os.path.dirname(__file__))
+OUTPUT_PATH = os.path.join(BASE_PATH, 'OUTPUT')
+
+CDI = 'cdi'
+ISO = 'iso'
+GDI = 'gdi'
 
 
 def gdi_file_formatter():
-    folders = os.listdir(os.curdir)
-    supported_formats = ('cdi', 'iso', 'gdi')
+    folders = os.listdir(BASE_PATH)
+    supported_formats = (CDI, ISO, GDI)
     input_files = []
-    folder_name = None
 
     if 'INPUT' not in folders:
         print('INPUT dir not found. Please make sure that INPUT folder exists')
@@ -17,48 +24,39 @@ def gdi_file_formatter():
         print('OUTPUT created in current directory')
 
     for supported_format in supported_formats:
-        input_files += (glob.glob(os.getcwd() + f'/I*/**/*.{supported_format}', recursive=True))
+        input_files += glob.glob(os.path.join(BASE_PATH, 'INPUT', f'**/*.{supported_format}'), recursive=True)
 
-    output_folders = os.listdir(os.path.join(os.curdir, 'OUTPUT'))
-    output_files = glob.glob(os.path.join(os.getcwd(), 'OUTPUT'), recursive=True)
+    output_folders = os.listdir(OUTPUT_PATH)
+    folder_number = 2  # folder 01 is reserved for GDEMU settings file
 
-    n = 2  # folder 01 is reserved for GDEMU settings file
-    for i in range(len(input_files)):
-        if n < 10:
-            if '0' + str(n) in output_folders:
-                while '0' + str(n) in output_folders:
-                    n += 1
-            folder_name = '0{}'.format(str(n))
-        if n >= 10:
-            if str(n) in output_folders:
-                while str(n) in output_folders:
-                    n += 1
-            folder_name = '{}'.format(str(n))
+    for input_file in input_files:
+        while str(folder_number).zfill(2) in output_folders:
+            folder_number += 1
+        folder_name = str(folder_number).zfill(2)
+        os.mkdir(os.path.join(OUTPUT_PATH, folder_name))
+        output_folders = os.listdir(OUTPUT_PATH)
+        file_format = input_file[-3:]
 
-        os.mkdir(os.path.join(output_files[0], folder_name))
-        output_folders = os.listdir(os.path.join(os.curdir, 'OUTPUT'))
-        file_format = input_files[i][-3:]
-
-        if file_format in ['cdi', 'iso']:
-            shutil.copy(input_files[i], os.path.join(output_files[0], folder_name, f"disc.{file_format}"))
+        if file_format in [CDI, ISO]:
+            shutil.copy(input_file, os.path.join(OUTPUT_PATH, folder_name, f'disc.{file_format}'))
             print('Moved {} to {}'
-                  .format(input_files[i], os.path.join(output_files[0], folder_name, f'disc.{file_format}')))
-        elif file_format in ['gdi']:
-            path = input_files[i]
+                  .format(input_file, os.path.join(OUTPUT_PATH, folder_name, f'disc.{file_format}')))
+        elif file_format == GDI:
+            path = input_file
             while path[-1] not in ('/', '\\'):
                 path = path[:-1]
             for file in os.listdir(path):
-                if os.path.isfile(os.path.join(path, file)) and not any(x in file for x in ('cdi', 'iso')):
-                    if file[-3:] == 'gdi':
-                        shutil.copy(os.path.join(path, file), os.path.join(output_files[0], folder_name, 'disc.gdi'))
+                if os.path.isfile(os.path.join(path, file)) and not file.endswith((CDI, ISO)):
+                    if file.endswith(GDI):
+                        shutil.copy(os.path.join(path, file), os.path.join(OUTPUT_PATH, folder_name, 'disc.gdi'))
                     else:
-                        shutil.copy(os.path.join(path, file), os.path.join(output_files[0], folder_name))
+                        shutil.copy(os.path.join(path, file), os.path.join(OUTPUT_PATH, folder_name))
 
                     print('Moving GDI file {} from {} to {}'
-                          .format(file, path, os.path.join(output_files[0], folder_name)))
+                          .format(file, path, os.path.join(OUTPUT_PATH, folder_name)))
         else:
-            n -= 1
-        n += 1
+            folder_number -= 1
+        folder_number += 1
 
 
 if __name__ == '__main__':
